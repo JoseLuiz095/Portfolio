86ce3635aedbea910085c252a911f7214e49c8fa
José Luiz <82607322+JoseLuiz095@users.noreply.github.com>
2024-02-25 11:34:22 -0300

    atualização
    com remoção de arquivos melhorando a performance

--- js/script.js
+++ js/script.js
@@ -1,74 +1,57 @@
-
 $(document).ready(function () {
+
+    // Smooth scroll on anchor click
     $("a").on('click', function (event) {
-        // animacão do botão
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
+        var hash = this.hash;
+        if (hash !== "") {
             event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 800, function () {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
             });
-        } // End if
+        }
     });
-    
-    // slide-up script
+
+    // Scroll to top button
     $('.scroll-up-btn').click(function () {
         $('html').animate({ scrollTop: 0 });
-        // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
+    // Smooth scroll on menu items click
     $('.navbarr .menu li a').click(function () {
-        // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
         $('.menu').toggleClass("teste");
     });
-    // toggle menu/navbarr script
 
+    // Toggle menu/navbar
     $('.menu-btn').click(function () {
-        $('.navbarr .menu').addClass("active");
+        $('.navbarr .menu').addClass("active").removeClass("teste");
         $('.menu-btn').addClass("none");
-        $('.navbarr .menu').removeClass("teste");
         $('.menu-btn i').toggleClass("active");
         $('.menu-btn1').removeClass("none");
-    })
+    });
+
     $('.menu-btn1').click(function () {
-        $('.navbarr .menu').removeClass("active");
-        $('.navbarr .menu').removeClass("teste");
+        $('.navbarr .menu').removeClass("active teste");
         $('.menu-btn i').toggleClass("active");
         $('.menu-btn1').addClass("none");
         $('.menu-btn').removeClass("none");
-    })
-
-
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: [" Desenvolvedor Front-end", " Técnico da informação", "Freelancer"],
-        typeSpeed: 200,
-        backSpeed: 60,
-        loop: true
-    });
-    var typed = new Typed(".typing-2", {
-        strings: [" Desenvolvedor Front-end", " Técnico da informação", "Freelancer"],
-        typeSpeed: 200,
-        backSpeed: 60,
-        loop: true
     });
 
+    // Typing text animation
+    var typingOptions = {
+        strings: ["Desenvolvedor de Sites", "Técnico da informação", "Programador"],
+        typeSpeed: 250,
+        backSpeed: 60,
+        loop: true
+    };
 
+    new Typed(".typing", typingOptions);
+    new Typed(".typing-2", typingOptions);
 
-    // owl carousel script
+    // Owl carousel
     $('.carousel').owlCarousel({
         margin: 25,
         loop: true,
@@ -76,20 +59,10 @@ $(document).ready(function () {
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0: {
-                items: 1,
-                nav: false
-            },
-            600: {
-                items: 2,
-                nav: false
-            },
-            1000: {
-                items: 3,
-                nav: false
-            }
+            0: { items: 1, nav: false },
+            600: { items: 2, nav: false },
+            1000: { items: 3, nav: false }
         }
     });
+
 });
-
-
